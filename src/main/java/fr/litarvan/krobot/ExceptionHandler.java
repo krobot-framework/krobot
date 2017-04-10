07032d307135ee97ec219bf4ba91bf95234dcd04
Adrien Navratil <adrien1975@live.fr>
2017-04-10 21:25:48 +0200

    License, publishing, deleted useless imports

--- src/main/java/fr/litarvan/krobot/ExceptionHandler.java
+++ src/main/java/fr/litarvan/krobot/ExceptionHandler.java
@@ -1,15 +1,31 @@
+/*
+ * Copyright 2017 Adrien "Litarvan" Navratil
+ *
+ * This file is part of Krobot.
+ *
+ * Krobot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Krobot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Krobot.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package fr.litarvan.krobot;
 
 import fr.litarvan.krobot.command.BadSyntaxException;
 import fr.litarvan.krobot.command.Command;
 import fr.litarvan.krobot.command.CommandContext;
-import fr.litarvan.krobot.command.SuppliedArgument;
 import fr.litarvan.krobot.command.UserNotFoundException;
 import fr.litarvan.krobot.util.Dialog;
 import fr.litarvan.krobot.util.Markdown;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 import net.dv8tion.jda.core.entities.PrivateChannel;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.logging.log4j.LogManager;
--- src/main/java/fr/litarvan/krobot/command/CommandGroup.java
+++ src/main/java/fr/litarvan/krobot/command/CommandGroup.java
@@ -1,7 +1,23 @@
+/*
+ * Copyright 2017 Adrien "Litarvan" Navratil
+ *
+ * This file is part of Krobot.
+ *
+ * Krobot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Krobot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Krobot.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package fr.litarvan.krobot.command;
 
-import fr.litarvan.krobot.command.Command;
-import fr.litarvan.krobot.command.Middleware;
 import java.util.List;
 
 public class CommandGroup
